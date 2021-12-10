ec51e4cdeaf56dc6a61a3d335d6861053c8e26c0
Mario <mamerida2013@gmail.com>
2021-12-10 19:27:24 -0300

    Mis primeros pasos en react native

---
+++ App.js
@@ -0,0 +1,26 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+const Texto = ()=>{
+  return(
+    <Text>Hola Mundo jejeje </Text>
+    )
+  
+}
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Texto/>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});

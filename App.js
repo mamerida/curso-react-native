d1840ddb12a79461ebefdacf67c01374e558b752
Mario <mamerida2013@gmail.com>
2021-12-11 17:31:38 -0300

    use of UseState and props in components

--- App.js
+++ App.js
@@ -1,23 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React,{useState} from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-const Texto = (props)=>{
-    const {children} = props;
-  return(
-    <Text> {children}</Text>
+
+// useState nos permite cambiar los estados dentro de los componentes para que puedan responder a algun evento
+
+const Texto = ()=>{
+
+    // En mi variable de estado se le coloca el nombre de la variable y la funcion que la actualiza junto con el valor inicial
+    const [Texto, setTexto] = useState("hola mundo");
+    //en este caso se ejecuta este codigo que cambia el texto mostrado mediante el useState declarado mas arriba
+    const actualizarTexto = () =>{
+      setTexto("Chao mundo cruel :C")
+    } 
+    return(
+        // La propiedad en press al momento de presionar el componente realiza una accion
+      <Text onPress={actualizarTexto}>{Texto}</Text>
     )
   
 }
 export default function App() {
   return (
     <View style={styles.container}>
-      <Texto>
-        Hola Mundo
-      </Texto>
-      {/* Al usar la propiedad children todo lo colocado entre las etiquetas se pasara como parametro*/}
-      <Texto>
-        Chao Mundo
-      </Texto>
+        <Texto />
       <StatusBar style="auto" />
     </View>
   );

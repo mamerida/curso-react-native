4a74f1fb67e47736235f36eed8772c218251bd69
Mario <mamerida2013@gmail.com>
2021-12-11 18:10:25 -0300

    Use of flex in stile and his consecuence in screen phone

--- App.js
+++ App.js
@@ -4,10 +4,10 @@ import { StyleSheet, Text, View } from 'react-native';
 
 // useState nos permite cambiar los estados dentro de los componentes para que puedan responder a algun evento
 
-const Texto = ()=>{
+const Texto = ({style})=>{
 
     // En mi variable de estado se le coloca el nombre de la variable y la funcion que la actualiza junto con el valor inicial
-    const [Texto, setTexto] = useState("hola mundo");
+    const [Texto, setTexto] = useState("Hola mundo");
     //en este caso se ejecuta este codigo que cambia el texto mostrado mediante el useState declarado mas arriba
     //amplie la funcionalidad de la funcion actualizar texto ahora lee el valor de Texto y dependiendo de eso cambia de una forma a otra
     const actualizarTexto = () =>{
@@ -23,7 +23,8 @@ const Texto = ()=>{
         //utilizo siempre componente Text para poder mostrar texto en pantalla 
         // style={{fontSize : 24}} una forma de definir estilos pero es mejor declararlo al final con el style
         // se usa {} y no {{}} por tratarse de una propiedad y no de un objeto
-      <Text style={styles.text} onPress={actualizarTexto}>{Texto}</Text>
+        // se le pueden pasar estilos o un array de estilos los cuales van a ser tomados de izquierda a derecha 
+      <Text style={[styles.text,style]} onPress={actualizarTexto}>{Texto}</Text>
     )
   
 }
@@ -32,7 +33,9 @@ export default function App() {
     //View nos permite mostrar los componentes mediante los children del mismo cambiar estados y manejar eventos como onPress 
     //No se pueden colocar otra cosa que no sean componentes dentro del View
     <View style={styles.container}>
-        <Texto />
+        <Texto style={styles.red}/>
+        <Texto style={styles.blue}/>
+        <Texto style={styles.green}/>
       <StatusBar style="auto" />
     </View>
   );
@@ -47,9 +50,24 @@ const styles = StyleSheet.create({
   },
   // creo el style text para poder llamarlo en mis componentes es como css dentro del componente
   text:{
-    color:'red',
+    // los numero colocados son unidades relativas y dependera a de la densidad de pixeles del telfono
+    color:'white',
     fontSize:24,
+    height:100,
+    width:100,
     // la propiedad css seria font-size pero en reactNative se cambio el - por camel case 
     // 'font-size':24,
+  },
+  red:{
+    flex:1,
+    backgroundColor:'red',
+  },
+  green:{
+    flex:1,
+    backgroundColor:'green',
+  },
+  blue:{
+    flex:1,
+    backgroundColor:'blue',
   },
 });

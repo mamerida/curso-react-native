8da6dde52424f7b7033c746f3b5025cf6bf06311
Mario <mamerida2013@gmail.com>
2021-12-11 17:13:09 -0300

    my first prop

--- App.js
+++ App.js
@@ -1,16 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-const Texto = ()=>{
+const Texto = (props)=>{
+    const {texto} = props;
   return(
-    <Text>Hola Mundo jejeje </Text>
+    <Text> {texto}</Text>
     )
   
 }
 export default function App() {
   return (
     <View style={styles.container}>
-      <Texto/>
+      <Texto texto={'Hola Mundo'}/>
+      <Texto texto={'Adios Mundo'}/>
       <StatusBar style="auto" />
     </View>
   );

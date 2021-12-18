d4bf300a9fae6cac569ce17141fe2863de7ce7bc
Mario <mamerida2013@gmail.com>
2021-12-18 18:26:59 -0300

    practice with sectionList and FlatList and api consumption

--- App.js
+++ App.js
@@ -1,37 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
-import React,{useState} from 'react';
+import React,{useEffect,useState} from 'react';
 import { StyleSheet, Text, View,Button, FlatList} from 'react-native'; // FlashList permite crear listas
 
 
 export default function App() {
-  
+  const [users, setUsers] = useState([]);
+  const [loading,setLoading] = useState(true); // al ponerlo en true habilito el logo al momento de cargar la aplicacion
+  //useEffect se utiliza para cuando tengo que solicitar resultados de un recurso que puede retornar un error
+ // el segundo elemento es un arreglo con las dependencias de useEfect
+  useEffect(()=>{
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json()) //lo primero se pasa la respuesta a json para poder trabajar con los datos  
+      .then(data => {
+        setUsers(data) // despues de haber sacalo la data la guardo en mi aplicacion y le quito la palabra cargando
+        setLoading(false)
+      })
+  },[]) 
 
+  if (loading){
+    return <View style={styles.center}><Text>Cargando...</Text></View>
+  }
   return (
     <View style={styles.container}>
-      <FlatList //FLATlIST MEJOR QUE SCROLL VIEW AL MOMENTO DE RENDERIZAR ELEMENTOS 
-      // datos a renderizar 
-      data={[
-        { key: 1 , name : "Nicolas" },
-        { key: 2 , name : "Marito"},
-        { key: 3 , name : "Chanchito Triste"},
-        { key: 4 , name : "Chanchito feliz"},
-        { key: 5 , name : "Tortita con manteca "},
-        { key: 6 , name : "Nicolas" },
-        { key: 7 , name : "Marito"},
-        { key: 8 , name : "Chanchito Triste"},
-        { key: 9 , name : "Chanchito feliz"},
-        { key: 10 , name : "Tortita con manteca "},
-        { key: 11 , name : "Nicolas" },
-        { key: 12 , name : "Marito"},
-        { key: 13 , name : "Chanchito Triste"},
-        { key: 14 , name : "Chanchito feliz"},
-        { key: 15 , name : "Tortita con manteca "},
-      ]} 
-      renderItem={({item}) => <Text style ={styles.item}>{item.name}</Text>}// como se va a renderizar el contenido mostrado 
-      // renderItem recibe un objeto por ende al aplicar {} lo desestructuro quedandome con el tiem entonces imprimo mediante text
+      <FlatList
+        data ={users} 
+        renderItem={({item})=><Text style={styles.item} >{item.name}</Text>}
+        keyExtractor={item => String(item.id)} // utilizo esta pripiedad para setear de manera manual las keys de los elementos 
       
       />
-      
     </View>
   );
 }
@@ -54,68 +50,172 @@ const styles = StyleSheet.create({
     backgroundColor:'#E9E9E9',
 
   },
+  center:{
+    flex:1,
+    alignItems:'center',
+    justifyContent:'center'
+  },
+
 
 
 });
 
+
+
+//---------- sections list
+
+// import { StatusBar } from 'expo-status-bar';
+// import React from 'react';
+// import { StyleSheet, Text, View,Button, SectionList} from 'react-native'; // FlashList permite crear listas
+
+
+// export default function App() {
+
+
+//   return (
+//     <View style={styles.container}>
+//       <SectionList
+//         // sections recibe arreglo con data a imprimir. Deben ser objetos con dos propiedades
+//           sections={[
+//             {title:'Grupo 1',
+//             data:[
+//               { key: 1 , name : "Nicolas" },
+//               { key: 2 , name : "Marito"},
+//               { key: 3 , name : "Chanchito Triste"},
+//               { key: 4 , name : "Chanchito feliz"},
+//               { key: 5 , name : "Tortita con manteca "},
+//           ]},
+//             {
+//               title:'Grupo 2',
+//               data:[
+//                 { key: 6 , name : "Nicolas" },
+//                 { key: 7 , name : "Marito"},
+//                 { key: 8 , name : "Chanchito Triste"},
+//                 { key: 9 , name : "Chanchito feliz"},
+//                 { key: 10 , name : "Tortita con manteca "},
+//             ]},
+//             {
+//               title:'Grupo 3',
+//               data:[
+//                 { key: 11 , name : "Nicola" },
+//                 { key: 12 , name : "Marito"},
+//                 { key: 13 , name : "Chanchito Triste"},
+//                 { key: 14 , name : "Chanchito feliz"},
+//                 { key: 15 , name : "Tortita con manteca "},
+//             ]}
+
+//         ]}
+//         renderItem={({item}) =><Text style={styles.item} >{item.name}</Text> }
+//         renderSectionHeader ={({section}) => <Text style={styles.section}>{section.title}</Text>}
+
+//       />
+
+//     </View>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+
+//   container:{
+//     flex:1,
+//     backgroundColor:'#fff',
+//     alignItems:'stretch', // stretch ocupa todo el ancho de la pantalla
+//     justifyContent:'center',
+//     paddingTop:22,
+//   },
+//   item:{
+//     padding:10,
+//     fontSize:22,
+//     height:50,
+//     borderBottomColor:'#ccc',
+//     borderBottomWidth: 1,
+//     backgroundColor:'#E9E9E9',
+
+//   },
+//   section:{
+//     fontSize:16,
+//     fontWeight:'bold',
+//     backgroundColor:'#ccc',
+//     paddingTop:2,
+//     paddingLeft:10,
+//     paddingRight:10,
+//     paddingBottom:2
+//   }
+
+
+// });
+
+
+
+
+//-----------
+
 // import { StatusBar } from 'expo-status-bar';
 // import React,{useState} from 'react';
 // import { StyleSheet, Text, View, TextInput, Dimensions,Button ,TouchableHighlight , TouchableNativeFeedback, TouchableOpacity} from 'react-native'; // importo textInput desde react native utilizo Dimensions para poder tomar el ancho del dispositivo
-// //TouchableNativeFeedback solo android 
+// //TouchableNativeFeedback solo android
 // //TouchableOpacity
 // const width = Dimensions.get('window').width // al get se le puede pasar window o screen se puede acceder a las propiedades width , heigth
 // export default function App() {
-  
+
 //   const[text,setText] = useState(' ')
 //   const[submited,setSubmit] = useState(' ')
 
 //   return (
-//     //View nos permite mostrar los componentes mediante los children del mismo cambiar estados y manejar eventos como onPress 
+//     //View nos permite mostrar los componentes mediante los children del mismo cambiar estados y manejar eventos como onPress
 //     //No se pueden colocar otra cosa que no sean componentes dentro del View
 //     <View style={styles.container}>
+//     <FlatList //FLATlIST MEJOR QUE SCROLL VIEW AL MOMENTO DE RENDERIZAR ELEMENTOS
+      // datos a renderizar
+      //data={}
+      //renderItem={({item}) => <Text style ={styles.item}>{item.name}</Text>}// como se va a renderizar el contenido mostrado
+      // renderItem recibe un objeto por ende al aplicar {} lo desestructuro quedandome con el tiem entonces imprimo mediante text
+
+      ///>
+
 //       <ScrollView>
 //       <Text>Texto : {submited}</Text>
-//       <TextInput 
-//       style={styles.input} 
-//       placeholder='   Ingrese su texto aqui ' 
+//       <TextInput
+//       style={styles.input}
+//       placeholder='   Ingrese su texto aqui '
 //       onChangeText={t => setText(t)}
 //       defaultValue={text}
 //       />
-//       <Button title='Aceptar' 
+//       <Button title='Aceptar'
 //       onPress={()=>{
 //         setSubmit(text)
 //         alert("Boton presionado correctamente")
 //         }}
 //       ></Button>
-//       <TouchableHighlight 
+//       <TouchableHighlight
 //       underlayColor={'#999'} //cambia el color al momento de presionar
 //       activeOpacity={0.2} //cambia la opacidad al momento de presionar
 //       onPress={()=>{
 //         setSubmit(text)
 //         alert("Boton presionado correctamente")
 //         }}
-//       > 
+//       >
 //       <Text>Aceptar Higtligth</Text>
 //       </TouchableHighlight>
 
-//       <TouchableOpacity 
+//       <TouchableOpacity
 //       style={styles.TouchableOpacity}
-//       onPress={()=>{    
+//       onPress={()=>{
 //         setSubmit(text)
 //         alert("Boton presionado correctamente")
 //         }}
-//       > 
+//       >
 //       <View style={styles.view}><Text>Aceptar Opacity</Text></View>
 //       </TouchableOpacity>
 
 
-//       <TouchableNativeFeedback 
+//       <TouchableNativeFeedback
 //       backgroundColor={TouchableNativeFeedback.Ripple('#00f',true)} // esto lo que hace es de la colores con una animacion al presionar el boton
 //       onPress={()=>{
 //         setSubmit(text)
 //         alert("Boton presionado correctamente")
 //         }}
-//       > 
+//       >
 //       <View style={styles.view}><Text>Aceptar Feedback</Text></View>
 //       </TouchableNativeFeedback>
 //       </ScrollView>
@@ -135,7 +235,7 @@ const styles = StyleSheet.create({
 //     height:40,
 //     width:'100%',
 //     // backgroundColor:'#eee',
-//     borderBottomColor:'#adc', // color de la linea inferior 
+//     borderBottomColor:'#adc', // color de la linea inferior
 //     borderBottomWidth: 1, //tamaño
 
 
@@ -147,7 +247,7 @@ const styles = StyleSheet.create({
 //     justifyContent:'center',
 //   },
 // scrollView:{
-//   width:Dimensions.get('window').width,  // para poder usar el 100% del ancho de la pantalla paso este estilo 
+//   width:Dimensions.get('window').width,  // para poder usar el 100% del ancho de la pantalla paso este estilo
 // }
 
 // });
@@ -174,17 +274,17 @@ const styles = StyleSheet.create({
 //       }else{
 //         setTexto("Hola mundo");
 //       }
-      
-//     } 
+
+//     }
 //     return(
-//         // La propiedad en press al momento de presionar el componente realiza una accion 
-//         //utilizo siempre componente Text para poder mostrar texto en pantalla 
+//         // La propiedad en press al momento de presionar el componente realiza una accion
+//         //utilizo siempre componente Text para poder mostrar texto en pantalla
 //         // style={{fontSize : 24}} una forma de definir estilos pero es mejor declararlo al final con el style
 //         // se usa {} y no {{}} por tratarse de una propiedad y no de un objeto
-//         // se le pueden pasar estilos o un array de estilos los cuales van a ser tomados de izquierda a derecha 
+//         // se le pueden pasar estilos o un array de estilos los cuales van a ser tomados de izquierda a derecha
 //       <Text style={[styles.text,style]} onPress={actualizarTexto}>{Texto}</Text>
 //     )
-  
+
 // }
 
   // container: {
@@ -192,7 +292,7 @@ const styles = StyleSheet.create({
   //   //indica la direccion en la cual se muestra el contenido row row-reverse column colum-reverse
   //   flexDirection:'column',
   //   backgroundColor: '#fff',
-  //   //esta propiedad alinea el contenido en el eje opuesto al declarado  flex-start flex-end center stretch 
+  //   //esta propiedad alinea el contenido en el eje opuesto al declarado  flex-start flex-end center stretch
   //   //baseline  (alto y ancho variable usa todo el contenido disponible)
   //   alignItems: 'flex-start',
   //   //justifyContent alinea contenido del elemento con el contenido flex-start flex-end center space-between space-around space-evenly
@@ -205,7 +305,7 @@ const styles = StyleSheet.create({
   //   fontSize:24,
   //   height:100,
   //   width:100,
-  //   // la propiedad css seria font-size pero en reactNative se cambio el - por camel case 
+  //   // la propiedad css seria font-size pero en reactNative se cambio el - por camel case
   //   // 'font-size':24,
   // },
   // red:{
